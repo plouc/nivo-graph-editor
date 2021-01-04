fa3661f4b2a50e6672f59af205d63ba9084b5dc1
plouc <benitteraphael@gmail.com>
2021-01-04 10:36:17 +0900

    Updates

--- static/js/7.4c5992b5.chunk.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 7.4c5992b5.chunk.js.LICENSE.txt */
-(this["webpackJsonpnivo-graph-editor"]=this["webpackJsonpnivo-graph-editor"]||[]).push([[7],{485:function(e,t,n){"use strict";n.r(t);var r=n(12),c=n(30);t.default=function(){var e=Object(c.a)(r.mark((function e(t){var n,c,i=arguments;return r.wrap((function(e){for(;;)switch(e.prev=e.next){case 0:n=i.length>1&&void 0!==i[1]?i[1]:{},(c=document.createElement("a")).download=n.fileName||"Untitled",c.href=URL.createObjectURL(t),c.addEventListener("click",(function(){setTimeout((function(){return URL.revokeObjectURL(c.href)}),3e4)})),c.click();case 3:case"end":return e.stop()}}),e)})));return function(t){return e.apply(this,arguments)}}()}}]);
-//# sourceMappingURL=7.4c5992b5.chunk.js.map
